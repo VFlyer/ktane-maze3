045b42dd28d60f3ec5a5273d4ac07ca05a728dde
luisdiogo98 <luisdiogo98@users.noreply.github.com>
2019-04-08 18:54:43 +0000

    Initial commit. Basic module look

---
+++ Assets/maze3Script.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using KModkit;
+
+public class maze3Script : MonoBehaviour 
+{
+	public KMBombInfo bomb;
+	public KMAudio Audio;
+
+	static int moduleIdCounter = 1;
+    int moduleId;
+    private bool moduleSolved;
+
+	void Awake()
+	{
+		moduleId = moduleIdCounter++;
+	}
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}
